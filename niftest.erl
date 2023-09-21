769d4939d2e425790bd7854876e8d8a5c5baa122
Andy Arvanitis <andyarvanitis@gmail.com>
2023-09-20 21:47:43 -0700

    WIP, but working

--- niftest.erl
+++ niftest.erl
@@ -1,16 +1,16 @@
 -module(niftest).
 
--export([init/0,hello/0]).
+-export([init/0,increment/1]).
 
--nifs([hello/0]).
+-nifs([increment/1]).
 
 -on_load(init/0).
 
 init() ->
       erlang:load_nif("./obj/development/Erlang_Nifs", 0).
 
-hello() ->
-      erlang:nif_error("NIF library not loaded").
-
-% increment(X) ->
+% hello() ->
 %       erlang:nif_error("NIF library not loaded").
+
+increment(_x) ->
+      erlang:nif_error("NIF library not loaded").
--- src/nif_bootstrap.cpp
+++ src/nif_bootstrap.cpp
@@ -12,7 +12,7 @@ using nif_fn_t = ERL_NIF_TERM (*)(ErlNifEnv* env, int argc, const ERL_NIF_TERM a
 extern "C" {
     extern void Erlang_Nifsinit();
     extern void Erlang_Nifsfinal();
-    // extern void populate_functions(ErlNifFunc * funcs, int cnt);
+    extern void populate_functions(ErlNifFunc * funcs, int cnt);
 } // extern "C"
 
 
@@ -21,11 +21,17 @@ static ERL_NIF_TERM hello(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     return enif_make_string(env, "Hello, world", ERL_NIF_LATIN1);
 }
 
-
-ErlNifFunc input_funcs[MAX_NIFS] =
+static ERL_NIF_TERM increment(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    { "hello", 0, hello, 0 }
-};
+    return enif_make_int(env, 2);
+}
+
+
+ErlNifFunc input_funcs[MAX_NIFS];
+
+// {
+//     { "increment", 1, increment, 0 }
+// };
 
 
 class niflist {
@@ -37,7 +43,7 @@ public:
 
         nif_funcs_internal[0] = input_funcs[0];
 
-        // populate_functions(nif_funcs_internal, MAX_NIFS);
+        populate_functions(nif_funcs_internal, MAX_NIFS);
 
         assert(sizeof(niflist) == sizeof(nif_funcs_internal));
     }
